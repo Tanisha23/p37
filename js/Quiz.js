97790fd335b010fa4613aeb37f2b500649f46b1e
Tanisha23 <77339985+Tanisha23@users.noreply.github.com>
2021-08-08 12:46:48 +0530

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,67 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    
+    question.hide(); 
+
+    background("Yellow");
+    fill(0);
+    textSize(30);
+    text("Result of the Quiz",340, 50);
+    text("----------------------------",320, 65);
+    Contestant.getPlayerInfo();
+    if(allContestants !== undefined){
+      debugger;
+      var display_Answers = 230;
+      fill("Blue");
+      textSize(20);
+      text("*NOTE: Contestant who answered correct are highlighted in green color!",130,230);
+
+      for(var plr in allContestants){
+        debugger;
+        var correctAns = "2";
+
+         if (correctAns !== allContestants[plr].answer){
+          fill("Green")
+          var display_position = 130;
+        }
+         else{
+           fill("red");
+           display_position+=20; 
+           textSize(15);
+          text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+         
+       }
+
+    }
+  }
+  }
+}
